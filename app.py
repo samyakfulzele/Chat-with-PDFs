bd775f883ed45d5073c797faf1b87c87054a62f2
samyakfulzele <110292543+samyakfulzele@users.noreply.github.com>
2025-10-11 01:03:51 +0530

    changes(10/10/25)

--- app.py
+++ app.py
@@ -1,115 +1,111 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain_community.vectorstores import FAISS
-import os
-import google.generativeai as genai
-from langchain.prompts import PromptTemplate
-from langchain.chains.question_answering import load_qa_chain
-from htmlTemplates import css, bot_template, user_template
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.embeddings import HuggingFaceEmbeddings
-
-load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
 def get_pdf_text(pdf_docs):
-    raw_text = ""
+    text = ""
     for pdf in pdf_docs:
-        reader = PdfReader(pdf)
-        for page in reader.pages:
-            raw_text += page.extract_text()
-    return raw_text
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
-def get_text_chunks(raw_text):
-    text_splitter = RecursiveCharacterTextSplitter(
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks = text_splitter.split_text(raw_text)
+    chunks = text_splitter.split_text(text)
     return chunks
 
+
 def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceEmbeddings()  # free, runs locally
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
-def get_conversational_chain():
-    prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}\n
-    Question: \n{question}\n
 
-    Answer:
-    """
+def get_conversation_chain(vectorstore):
+    # llm = ChatOpenAI()
+    llm = HuggingFaceHub(
+        repo_id="google/flan-t5-xl",
+        model_kwargs={
+            "temperature": 0.5,
+            "max_length": 512,
+            "torch_dtype": "auto",
+            "low_cpu_mem_usage": True
+        }
+    )
 
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.3)
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
 
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-
-    return chain
 
 def handle_userinput(user_question):
-    embeddings = HuggingFaceEmbeddings()
-    
-    if not os.path.exists("faiss_index"):
-        st.warning("Please upload and process PDFs first!")
-        return
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
 
-
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_question)
-
-    chain = get_conversational_chain()
-
-    
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
-
-    if "chat_history" not in st.session_state:
-        st.session_state['chat_history'] = []
-
-    st.session_state['chat_history'].append(user_question)
-    st.session_state['chat_history'].append(response['output_text'])
-
-    for i, msg in enumerate(st.session_state['chat_history']):
+    for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}",msg), unsafe_allow_html=True)
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace("{{MSG}}",msg), unsafe_allow_html=True)
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
-def main(): 
-    st.set_page_config(page_title="Chat with PDF", page_icon=":books:", layout="wide")
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
     if "chat_history" not in st.session_state:
-        st.session_state['chat_history'] = []
+        st.session_state.chat_history = None
 
-    st.header("Chat with PDF :books:")
-    user_question = st.text_input("Ask Question From Your PDFs")
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
 
     with st.sidebar:
-        st.subheader("Your Documents")
-        pdf_docs = st.file_uploader("Upload your PDFs", type=["pdf"], accept_multiple_files=True)
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
-            with st.spinner("Processing PDFs"):
+            with st.spinner("Processing"):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                # get text chunks
-                text_chunks = get_text_chunks(raw_text)
-                # create vector store
-                get_vectorstore(text_chunks)
-                st.success("Done")
 
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
 
 
 if __name__ == '__main__':
