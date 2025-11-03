1706afc1d346485964b77e0be167a805165bdb73
Samyak Baliram Fulzele <110292543+samyakfulzele@users.noreply.github.com>
2025-11-03 18:34:52 +0530

    Add import for HuggingFaceInstructEmbeddings

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
-
+ 
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
