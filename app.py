e65b89cc3965d70a463b9eb97e519bc504e71a29
Samyak Baliram Fulzele <110292543+samyakfulzele@users.noreply.github.com>
2025-11-03 22:34:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 
-load_dotenv()
+# load_dotenv()
 
 genai.configure(api_key="AIzaSyB6gBXTdPKdcm9xGnd5WvSan8OoMCM0MQg")
 
